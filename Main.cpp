baa092921541dbf1879751026d6d2520d579ac24
Alexandre K <alexandre.kcz.pro@gmail.com>
2023-09-03 21:16:49 +0200

    Import mesh fonctionnel a moitié

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,8 @@
-#include"Mesh.h"
+#include "Model.h"
 
 #include <wtypes.h> //pour DWORD pour le gpu nvidia
 /* pour activer le gpu nvidia ? (les uv bug bruh)*/
+
 extern "C"
 {
 	//__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
@@ -10,139 +11,6 @@ extern "C"
 
 const unsigned int width = 800;
 const unsigned int height = 800;
-
-
-Vertex vertices[] =
-{
-	Vertex{glm::vec3(-1.0f, 0.0f,  1.0f), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.0f, 0.0f)},
-	Vertex{glm::vec3(-1.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.0f, 1.0f)},
-	Vertex{glm::vec3(1.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(1.0f, 1.0f)},
-	Vertex{glm::vec3(1.0f, 0.0f,  1.0f), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(1.0f, 0.0f)}
-};
-
-GLuint indices[] =
-{
-	0, 1, 2,
-	0, 2, 3
-};
-
-Vertex lightVertices[] =
-{ //     COORDINATES     //
-	Vertex{glm::vec3(-0.1f, -0.1f,  0.1f)},
-	Vertex{glm::vec3(-0.1f, -0.1f, -0.1f)},
-	Vertex{glm::vec3(0.1f, -0.1f, -0.1f)},
-	Vertex{glm::vec3(0.1f, -0.1f,  0.1f)},
-	Vertex{glm::vec3(-0.1f,  0.1f,  0.1f)},
-	Vertex{glm::vec3(-0.1f,  0.1f, -0.1f)},
-	Vertex{glm::vec3(0.1f,  0.1f, -0.1f)},
-	Vertex{glm::vec3(0.1f,  0.1f,  0.1f)}
-};
-
-GLuint lightIndices[] =
-{
-	0, 1, 2,
-	0, 2, 3,
-	0, 4, 7,
-	0, 7, 3,
-	3, 7, 6,
-	3, 6, 2,
-	2, 6, 5,
-	2, 5, 1,
-	1, 5, 4,
-	1, 4, 0,
-	4, 5, 6,
-	4, 6, 7
-};
-
-//Plane Old 
-/*
-GLfloat vertices[] = {
-	-1.0f,	0.0f,	1.0f,		0.0f,	0.0f,	0.0f,		0.0f,	0.0f,		0.0f,	1.0f,	0.0f,
-	-1.0f,	0.0f,	-1.0f,		0.0f,	0.0f,	0.0f,		0.0f,	1.0f,		0.0f,	1.0f,	0.0f,
-	1.0f,	0.0f,	-1.0f,		0.0f,	0.0f,	0.0f,		1.0f,	1.0f,		0.0f,	1.0f,	0.0f,
-	1.0f,	0.0f,	1.0f,		0.0f,	0.0f,	0.0f,		1.0f,	0.0f,		0.0f,	1.0f,	0.0f
-};
-
-GLuint indices[] = {
-	0,1,2,
-	0,2,3
-};*/
-
-//Coordonees des vertices (il piramido)
-/*
-GLfloat vertices[] = {*/
-	/* Coordonees :				Couleurs : 					UV:					Normals : *//*
-	-0.5f,	0.0f,	0.5f,		0.83f, 0.70f, 0.44f,		0.0f,	0.0f,		0.0f, -1.0f, 0.0f,
-	-0.5f,	0.0f,	-0.5f,		0.83f, 0.70f, 0.44f,		0.0f,	5.0f,		0.0f, -1.0f, 0.0f,
-	0.5f,	0.0f,	-0.5f,		0.83f, 0.70f, 0.44f,		5.0f,	5.0f,		0.0f, -1.0f, 0.0f,
-	0.5f,	0.0f,	0.5f,		0.83f, 0.70f, 0.44f,		5.0f,	0.0f,		0.0f, -1.0f, 0.0f,
-
-	-0.5f,	0.0f,	0.5f,		0.83f, 0.70f, 0.44f,		0.0f,	0.0f,		-0.8f, 0.5f, 0.0f,
-	-0.5f,	0.0f,	-0.5f,		0.83f, 0.70f, 0.44f,		5.0f,	0.0f,		-0.8f, 0.5f, 0.0f,
-	0.0f,	0.8f,	0.0f,		0.92f, 0.86f, 0.76f,		2.5f,	5.0f,		-0.8f, 0.5f, 0.0f,
-
-	-0.5f,	0.0f,	-0.5f,		0.83f, 0.70f, 0.44f,		5.0f,	0.0f,		0.0f, 0.5f, -0.8f,
-	0.5f,	0.0f,	-0.5f,		0.83f, 0.70f, 0.44f,		0.0f,	0.0f,		0.0f, 0.5f, -0.8f,
-	0.0f,	0.8f,	0.0f,		0.92f, 0.86f, 0.76f,		2.5f,	5.0f,		0.0f, 0.5f, -0.8f,
-
-	0.5f,	0.0f,	-0.5f,		0.83f, 0.70f, 0.44f,		0.0f,	0.0f,		0.8f, 0.5f, 0.0f,
-	0.5f,	0.0f,	0.5f,		0.83f, 0.70f, 0.44f,		5.0f,	0.0f,		0.8f, 0.5f, 0.0f,
-	0.0f,	0.8f,	0.0f,		0.92f, 0.86f, 0.76f,		2.5f,	5.0f,		0.8f, 0.5f, 0.0f,
-
-	0.5f,	0.0f,	0.5f,		0.83f, 0.70f, 0.44f,		5.0f,	0.0f,		0.0f, 0.5f, 0.8f,
-	-0.5f,	0.0f,	0.5f,		0.83f, 0.70f, 0.44f,		0.0f,	0.0f,		0.0f, 0.5f, 0.8f,
-	0.0f,	0.8f,	0.0f,		0.92f, 0.86f, 0.76f,		2.5f,	5.0f,		0.0f, 0.5f, 0.8f,
-};
-
-GLuint indices[] = {
-	0,1,2,
-	0,2,3,
-	4,6,5,
-	7,9,8,
-	10,12,11,
-	13,15,14
-};*/
-
-/*
-GLfloat lightVertices[] = {
-	-0.1f, -0.1f, 0.1f,
-	-0.1f, -0.1f, -0.1f,
-	0.1f, -0.1f, -0.1f,
-	0.1f, -0.1f, 0.1f,
-	-0.1f, 0.1f, 0.1f,
-	-0.1f, 0.1f, -0.1f,
-	0.1f, 0.1f, -0.1f,
-	0.1f, 0.1f, 0.1f
-};
-
-GLuint lightIndices[] = {
-	0, 1, 2,
-	0, 2, 3,
-	0, 4, 7,
-	0, 7, 3,
-	3, 7, 6,
-	3, 6, 2,
-	2, 6, 5,
-	2, 5, 1,
-	1, 5, 4,
-	1, 4, 0,
-	4, 5, 6,
-	4, 6, 7
-};*/
-
-/* QUAD
-GLfloat vertices[] = {
-	-0.5f,	-0.5f,	0.0f,		0.83f, 0.70f, 0.44f,	0.0f,	0.0f,
-	-0.5f,	0.5f,	0.0f,		0.83f, 0.70f, 0.44f,	0.0f,	1.0f,
-	0.5f,	0.5f,	0.0f,		0.83f, 0.70f, 0.44f,	1.0f,	1.0f,
-	0.5f,	-0.5f,	0.0f,		0.83f, 0.70f, 0.44f,	1.0f,	0.0f,
-};
-
-GLuint indices[] = {
-	0, 2, 1,
-	0, 3, 2,
-};*/
-
 
 int main() {
 
@@ -168,41 +36,18 @@ int main() {
 
 	glViewport(0, 0, width, height);
 
-	Texture textures[]
-	{
-		Texture("Vittor.png", "diffuse", 0, GL_RGB, GL_UNSIGNED_BYTE),
-		Texture("Vittor_Specular.png", "specular", 1, GL_RED, GL_UNSIGNED_BYTE)
-	};
+
 
 	Shader shaderProgram("default.vert", "default.frag");
 
-	std::vector <Vertex> verts(vertices, vertices + sizeof(vertices) / sizeof(Vertex));
-	std::vector <GLuint> ind(indices, indices + sizeof(indices) / sizeof(GLuint));
-	std::vector <Texture> tex(textures, textures + sizeof(textures) / sizeof(Texture));
-	Mesh floor(verts, ind, tex);
-	
-
-	Shader lightShader("light.vert", "light.frag");
-
-	std::vector <Vertex> lightVerts(lightVertices, lightVertices + sizeof(lightVertices) / sizeof(Vertex));
-	std::vector <GLuint> lightInd(lightIndices, lightIndices + sizeof(lightIndices) / sizeof(GLuint));
-	Mesh light(lightVerts, lightInd, tex);
+	//Shader lightShader("light.vert", "light.frag");
 
 	glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
-
 	glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
 	glm::mat4 lightModel = glm::mat4(1.0f);
 	lightModel = glm::translate(lightModel, lightPos);
 
-	glm::vec3 pyramidPos = glm::vec3(0.0f, 0.0f, 0.0f);
-	glm::mat4 pyramidModel = glm::mat4(1.0f);
-	pyramidModel = glm::translate(pyramidModel, pyramidPos);
-
-	lightShader.Activate();
-	glUniformMatrix4fv(glGetUniformLocation(lightShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(lightModel));
-	glUniform4f(glGetUniformLocation(lightShader.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
 	shaderProgram.Activate();
-	glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "model"), 1, GL_FALSE, glm::value_ptr(pyramidModel));
 	glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
 	glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
 
@@ -218,6 +63,8 @@ int main() {
 	glEnable(GL_DEPTH_TEST);
 
 	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
+
+	Model model("models/scene.gltf");
 
 	while (!glfwWindowShouldClose(window)) {
 
@@ -241,8 +88,7 @@ int main() {
 
 		camera.updateMatrix(45.0f, 0.1f, 100.0f);
 
-		floor.Draw(shaderProgram, camera);
-		light.Draw(lightShader, camera);
+		model.Draw(shaderProgram, camera);
 
 		glfwSwapBuffers(window);
 
@@ -250,7 +96,7 @@ int main() {
 	}
 
 	shaderProgram.Delete();
-	lightShader.Delete();
+	//lightShader.Delete();
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
