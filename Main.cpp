3653d5d1937754cdde82ae9cf20834fd5ff6e709
Alexandre K <alexandre.kcz.pro@gmail.com>
2023-09-04 00:02:53 +0200

    Moai qui charge

--- Main.cpp
+++ Main.cpp
@@ -64,7 +64,7 @@ int main() {
 
 	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
 
-	Model model("models/scene.gltf");
+	Model model("models/moai/moyMagique.gltf");
 
 	while (!glfwWindowShouldClose(window)) {
 
